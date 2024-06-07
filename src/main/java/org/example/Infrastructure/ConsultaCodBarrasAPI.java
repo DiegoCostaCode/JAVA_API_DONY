5f8f4707c6d61f564951816a4e6c6ebe544ce12a
Diego <diegocosta.contato2021@gmail.com>
2024-06-07 11:27:37 -0300

    Adicionando While na consulta de CodBarras, e puxando novos materiais do banco de dados

--- src/main/java/org/example/Infrastructure/ConsultaCodBarrasAPI.java
+++ src/main/java/org/example/Infrastructure/ConsultaCodBarrasAPI.java
@@ -34,95 +34,106 @@ public class ConsultaCodBarrasAPI extends UsuarioRepository implements Loggable<
         List<Usuario> usuarios = usuarioRepository.Read();
         List<Material> materiais = materialRepository.Read();
 
-        System.out.print("Digite o seu ID: ");
-        int Id = scanner.nextInt();
+        var next = true;
 
-        Usuario usuarioID = null;
+        while(next){
 
-        usuarioID = usuarios.stream()
-                .filter(u -> u.getId_entidade() == Id)
-                .findFirst()
-                .orElse(null);
+            System.out.print("Digite o seu ID: ");
+            int Id = scanner.nextInt();
 
-        if (usuarioID == null) {
-            Loggable.logInfo("ID inválido. Por favor, se cadastre para cadastrar produtos.");
-            return;
-        }
+            Usuario usuarioID = null;
 
-        System.out.println("Bem-vindo " + usuarioID.getNome_usuario());
+            usuarioID = usuarios.stream()
+                    .filter(u -> u.getId_entidade() == Id)
+                    .findFirst()
+                    .orElse(null);
 
-        System.out.print("De 1 a 3, por favor, escolha a categoria do produto reciclável: \n 1 - Plástico;\n 2 - Lata;\n 3 - Vidro;\n");
-        int category = scanner.nextInt();
-        scanner.nextLine();
-
-        Material material = null;
-        switch (category) {
-            case 1:
-                material = materiais.stream().filter(m -> m.getId_entidade() == 1).findFirst().orElse(null);
-                break;
-            case 2:
-                material = materiais.stream().filter(m -> m.getId_entidade() == 2).findFirst().orElse(null);
-                break;
-            case 3:
-                material = materiais.stream().filter(m -> m.getId_entidade() == 3).findFirst().orElse(null);
-                break;
-            default:
-                Loggable.logInfo("Material não encontrado. Por favor, escolha novamente");
+            if (usuarioID == null) {
+                Loggable.logInfo("ID inválido. Por favor, se cadastre para cadastrar produtos.");
                 return;
-        }
+            }
 
-        if (material == null) {
-            System.out.println("Material não encontrado. Por favor, escolha novamente.");
-            return;
-        }
+            System.out.println("Bem-vindo " + usuarioID.getNome_usuario());
+
+            System.out.print("De 1 a 5 por favor, escolha a categoria do produto reciclável: \n 1 - Plástico;\n 2 - Lata;\n 3 - Vidro;\n4 - Papel;\n5 - Tecido algodão;\n");
+            int category = scanner.nextInt();
+            scanner.nextLine();
+
+            Material material = null;
+            switch (category) {
+                case 1:
+                    material = materiais.stream().filter(m -> m.getId_entidade() == 4).findFirst().orElse(null);
+                    break;
+                case 2:
+                    material = materiais.stream().filter(m -> m.getId_entidade() == 2).findFirst().orElse(null);
+                    break;
+                case 3:
+                    material = materiais.stream().filter(m -> m.getId_entidade() == 6).findFirst().orElse(null);
+                    break;
+                case 4:
+                    material = materiais.stream().filter(m -> m.getId_entidade() == 7).findFirst().orElse(null);
+                    break;
+                case 5:
+                    material = materiais.stream().filter(m -> m.getId_entidade() == 8).findFirst().orElse(null);
+                    break;
+                default:
+                    Loggable.logInfo("Material não encontrado. Por favor, escolha novamente");
+                    return;
+            }
+
+            if (material == null) {
+                System.out.println("Material não encontrado. Por favor, escolha novamente.");
+                return;
+            }
 
 
-        System.out.print("Digite o código de barras (GTIN): ");
-        String gtin = scanner.nextLine();
+            System.out.print("Digite o código de barras (GTIN): ");
+            String gtin = scanner.nextLine();
 
-        String url = "https://api.cosmos.bluesoft.com.br/gtins/" + gtin;
+            String url = "https://api.cosmos.bluesoft.com.br/gtins/" + gtin;
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .header("User-Agent", "Cosmos-API-Request")
-                .header("X-Cosmos-Token", "DADkHujMMbPF0EEvsTLFgQ")
-                .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .header("User-Agent", "Cosmos-API-Request")
+                    .header("X-Cosmos-Token", "DADkHujMMbPF0EEvsTLFgQ")
+                    .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode()!= 200) {
-            System.out.println("Erro ao consultar o código de barras. Código de status: " + response.statusCode());
-            return;
-        }
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode()!= 200) {
+                System.out.println("Erro ao consultar o código de barras. Código de status: " + response.statusCode());
+                return;
+            }
 
-        String responseBody = response.body();
+            String responseBody = response.body();
 
-        JsonNode jsonNode;
-        try {
-            jsonNode = mapper.readTree(responseBody);
-        } catch (JsonParseException e) {
-            Loggable.logInfo("Erro ao parsear o JSON: " + e.getMessage());
-            return;
-        }
+            JsonNode jsonNode;
+            try {
+                jsonNode = mapper.readTree(responseBody);
+            } catch (JsonParseException e) {
+                Loggable.logInfo("Erro ao parsear o JSON: " + e.getMessage());
+                return;
+            }
 
-        String description = jsonNode.get("description").asText();
-        String gtinValue = jsonNode.get("gtin").asText();
-        String thumbnail = jsonNode.get("thumbnail").asText();
+            String description = jsonNode.get("description").asText();
+            String gtinValue = jsonNode.get("gtin").asText();
+            String thumbnail = jsonNode.get("thumbnail").asText();
 
-        reciclagem.setTitulo(description);
-        reciclagem.setCod_barras(gtinValue);
-        reciclagem.setThumbnail(thumbnail);
-        reciclagem.setMaterial_id(material);
-        reciclagem.setUsuario_id(usuarioID);
+            reciclagem.setTitulo(description);
+            reciclagem.setCod_barras(gtinValue);
+            reciclagem.setThumbnail(thumbnail);
+            reciclagem.setMaterial_id(material);
+            reciclagem.setUsuario_id(usuarioID);
 
-        reciclagemRepository.Create(reciclagem);
+            reciclagemRepository.Create(reciclagem);
 
-        System.out.println("Produto criado:");
-        System.out.println("Título: " + reciclagem.getTitulo());
-        System.out.println("Código de barras: " + reciclagem.getCod_barras());
-        System.out.println("Thumbnail: " + reciclagem.getThumbnail());
-        System.out.println("Usuário: " + reciclagem.getUsuario_id().getId_entidade());
-        System.out.println("Material: " + reciclagem.getMaterial_id().getNome_material());
+            System.out.println("Produto criado:");
+            System.out.println("Título: " + reciclagem.getTitulo());
+            System.out.println("Código de barras: " + reciclagem.getCod_barras());
+            System.out.println("Thumbnail: " + reciclagem.getThumbnail());
+            System.out.println("Usuário: " + reciclagem.getUsuario_id().getId_entidade());
+            System.out.println("Material: " + reciclagem.getMaterial_id().getNome_material());
+            }
     }
 }
